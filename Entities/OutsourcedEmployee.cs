6f6e1eb82b05f64caa3669e44e08ad00e32b8ee3
Mario Nogueira <48410549+supramatti@users.noreply.github.com>
2020-10-16 20:31:26 -0300

    Add files via upload

---
+++ Entities/OutsourcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ConsoleApp10_1.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) 
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return Hours * ValuePerHour + (AdditionalCharge * 1.1);
+        }
+    }
+}
